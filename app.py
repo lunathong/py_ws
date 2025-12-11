a5079554eb9ddb2f2fd04e48bffd0447e52092f7
luna.thong <lunathongl@gmail.com>
2025-12-11 20:22:22 +0800

    host 0.0.0.0

--- app.py
+++ app.py
@@ -104,4 +104,4 @@ def update_monitor_data():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
